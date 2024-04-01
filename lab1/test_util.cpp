48db56bf9eeb2ddb3aca4b4c854e7240cc1422d7
Minguk Choi <koreachoi96@gmail.com>
2024-04-01 15:40:54 +0900

    Update test_util.cpp

--- lab1/test_util.cpp
+++ lab1/test_util.cpp
@@ -204,7 +204,6 @@ void SchedulerTest::check_answer(const std::string& scheduler_name, std::string 
     } else {
       Job* data = end_jobs_.data();
       size_t data_size = end_jobs_.size();
-      std::cout << data_size << std::endl;
       SHA256_Update(&sha256, data, data_size*sizeof(Job));
     }
 
